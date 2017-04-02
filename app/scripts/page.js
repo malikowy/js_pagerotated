ceb8dbdaa91423009809bb5285c92664e09305eb
Malikowy <gabriel.ignatiuk@gmail.com>
2017-04-02 22:25:15 +0200

    fixed import links in sass, better view of content

--- app/scripts/page.js
+++ app/scripts/page.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
-  console.log("page.js loaded");
-  $('#main-div').show("fast");
+  console.log('page.js loaded');
+  $('#main-div').show('fast');
   $('ul').on('click', '#main', function () {
-    $('.content').hide("fast");
-    $('#main-div').show("slow");
+    $('.content').hide('fast');
+    $('#main-div').show('slow');
   });
   $('ul').on('click', '#about', function () {
-    $('.content').hide("fast");
-    $('#about-div').show("slow");
+    $('.content').hide('fast');
+    $('#about-div').show('slow');
   });
   $('ul').on('click', '#contact', function () {
-    $('.content').hide("fast");
-    $('#contact-div').show("slow");
+    $('.content').hide('fast');
+    $('#contact-div').show('slow');
   });
 });
